d1d451ef0d71d48a0921b88a73b0bb020b5511a0
keranbarbieri <keranbarbieri@hotmail.com>
2026-06-17 18:39:19 -0300

    feat: add conversation looping in chat

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,22 @@
+# Cria o loop no terminal para eu conversar com o sistema.
 from search import search_prompt
 
 def main():
-    chain = search_prompt()
+    print("--- Bem-vindo ao Chat SuperTech ---")
+    ask_function = search_prompt()
 
-    if not chain:
+    if not ask_function:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
-    pass
+    while True:
+        pergunta = input("\nPERGUNTA: ")
+        if pergunta.lower() in ["sair", "exit", "quit"]:
+            break
+        
+        resposta = ask_function(pergunta)
+        print(f"RESPOSTA: {resposta}")
+        
 
 if __name__ == "__main__":
     main()
